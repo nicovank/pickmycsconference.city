92f4594da46881e130a77dc90f04c899d3c8b0f3
Maya V. Nedkova <mayavnedkova@gmail.com>
2025-06-23 13:11:35 -0400

    First Page Extraction and requirements
    Summary:
    
    Includes two files, a requirements.txt with PyPDF2 and an updated extract_first_page_of_pdf.py that now takes arguments from command line (path of pdf to extract first page of and output path).
    
    Test Plan:
    
    I briefly tested it with two local files on my computer. I have not tested these edge cases:
    1. Pdf does not exist
    2. Pdf with no pages
    3. Pdf with 1 page
    4. Invalid output path

---
+++ src/python/extract_first_page_of_pdf.py
@@ -0,0 +1,34 @@
+import sys
+from PyPDF2 import PdfReader, PdfWriter
+
+
+def extract_first_page_of_pdf(pdf: str, desired_path: str) -> None:
+    """
+    Extracts the first page of a PDF file and saves it to a new file.
+
+    Args:
+        pdf (str): The path to the input PDF file.
+        desired_path (str): The path where the first page will be saved.
+    """
+
+    reader = PdfReader(pdf)
+    writer = PdfWriter()
+    base_name = pdf.replace(".pdf", "")
+
+    # Add the first page to the writer
+    if len(reader.pages) > 0:
+        writer.add_page(reader.pages[0])
+
+    # Write the first page to the desired path
+    with open(desired_path, "wb") as output_pdf:
+        writer.write(output_pdf)
+    print(f"First page of {base_name} saved to {desired_path}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print(
+            "Usage: python extract_first_page_of_pdf.py <input_pdf_path> <output_pdf_path>"
+        )
+        sys.exit(1)
+    extract_first_page_of_pdf(sys.argv[1], sys.argv[2])
