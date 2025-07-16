9da1daf2149137c2441f7ebf0d9607d4a51be451
Maya V. Nedkova <mayavnedkova@gmail.com>
2025-07-16 17:15:14 -0400

    Get_Coords_From_Affiliation
    Includes one function get_coords_from_affiliation which takes an affiliation as a string as its only parameter, calls the geocoord api, and returns a tuple with the latitude and longitude.
    
    Notable mention, this API also allows for the calculation of distance between locations which may be useful in the future.
    
    Fixed typing issue.

---
+++ src/python/get_coords_from_affiliation.py
@@ -0,0 +1,25 @@
+import sys
+import json
+import os
+import geopy
+from geopy.geocoders import GoogleV3
+
+api_key = os.getenv("GEOCODING_API_KEY")
+geolocator = GoogleV3(api_key)
+
+
+def get_coords_from_affiliation(affiliation: str) -> tuple[str, str] | str:
+    location = geolocator.geocode(affiliation)
+    if location:
+        return json.dumps(
+            {"latitude": location.latitude, "longitude": location.longitude}
+        )
+    else:
+        return json.dumps({"error": "Location not found"})
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python get_coords_from_affiliation.py <affiliation string>")
+        sys.exit(1)
+    print(get_coords_from_affiliation(sys.argv[1]))
