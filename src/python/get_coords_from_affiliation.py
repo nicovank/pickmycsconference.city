95afa2445dc491d37bdfb188f25f2e8e6db84e49
mnedkova <maya.nedkova@live.com>
2025-07-21 15:42:55 -0400

    Get_Coords_From_Affiliation (#12)
    * First Iteration of Cluster Map
    
    A cluster map with 3 geo-coordinates near Albany.
    
    Reference:
    https://www.youtube.com/watch?v=xHmS4OpcEOc
    
    Fixed formatting
    
    * Get_Coords_From_Affiliation
    
    Includes one function get_coords_from_affiliation which takes an affiliation as a string as its only parameter, calls the geocoord api, and returns a tuple with the latitude and longitude.
    
    Notable mention, this API also allows for the calculation of distance between locations which may be useful in the future.

---
+++ src/python/get_coords_from_affiliation.py
@@ -0,0 +1,22 @@
+import sys
+import json
+import os
+from geopy.geocoders import GoogleV3  # type: ignore
+
+api_key = os.getenv("GEOCODING_API_KEY")
+geolocator = GoogleV3(api_key)
+
+
+def get_coords_from_affiliation(affiliation: str) -> tuple[float, float]:
+    location = geolocator.geocode(affiliation)
+    if location:
+        return location.latitude, location.longitude
+    else:
+        raise Exception("Location not found")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python get_coords_from_affiliation.py <affiliation string>")
+        sys.exit(1)
+    print(get_coords_from_affiliation(sys.argv[1]))
