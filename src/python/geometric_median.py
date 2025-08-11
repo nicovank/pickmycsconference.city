986fa6f28d6b2b39e35e77e40296c76623395750
Maya V. Nedkova <mayavnedkova@gmail.com>
2025-08-11 09:48:18 -0400

    Get geometric median
    Takes a JSON file path as an argument and calculates the Geometric median (point that minimizes the sum of distances) using the Nelder-Mead algorithm provided by scipy. Returns the longitude and latitude of this point as a tuple and the total miles.

---
+++ src/python/geometric_median.py
@@ -0,0 +1,52 @@
+import sys
+import json
+import numpy as np
+from geopy.distance import geodesic
+from scipy.optimize import minimize
+
+cities = json.loads(open("../www/sample.json").read())
+
+
+def get_geometric_median(JSON_path: str) -> tuple:
+    """
+    Calculate the geometric median of a set of geographical coordinates using the Nelder-Mead algorithm: minimizes sum of distances to all points.
+
+    Args:
+        cities (list): A list of dictionaries, each containing latitude and longitude.
+
+    Returns:
+        tuple: A tuple containing the latitude and longitude of the geometric median.
+    """
+
+    cities = json.loads(open(JSON_path).read())
+
+    coords = [(c["latitude"], c["longitude"]) for c in cities]
+
+    lat0 = np.mean([latitude for latitude, _ in coords])
+    lon0 = np.mean([longitude for _, longitude in coords])
+    initial_guess = np.array([lat0, lon0])
+
+    def get_total_distance(point) -> float:
+        """
+        Calculate the total distance from a point to all coordinates.
+
+        Args:
+            point (tuple): A tuple containing latitude and longitude calculated by scipy.optimize.minimize.
+
+        Returns:
+            float: The total distance from the point to all coordinates.
+        """
+        return sum(geodesic(point, coord).miles for coord in coords)
+
+    result = minimize(get_total_distance, initial_guess, method="Nelder-Mead")
+    return result.x, result.fun
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <path_to_json>")
+        sys.exit(1)
+    JSON_path = sys.argv[1]
+    median_coords, total_distance = get_geometric_median(JSON_path)
+    print(f"Geometric Median Coordinates: {median_coords}")
+    print(f"Total Distance: {total_distance} miles")
