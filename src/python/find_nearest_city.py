654cf5b284bbcbd8e444e93da3db745a8887cf8a
Maya V. Nedkova <mayavnedkova@gmail.com>
2025-08-11 17:20:39 -0400

    Find nearest city
    Function find_nearest_city.py takes the Geometric median coordinates calculated by geometric_median.py with JSON_file input and compares distance between each city's coordinates in worldcities.csv and the Geometric Median to find the closest city. Returns a dictionary containing city name, country, longitude, latitude, and distance.
    
    Source for worldcities.csv: http://simplemaps.com/data/world-cities

---
+++ src/python/find_nearest_city.py
@@ -0,0 +1,43 @@
+import sys
+import csv
+from geometric_median import get_geometric_median
+from geopy.distance import geodesic  # type: ignore
+
+
+def find_nearest_city(target_coords: tuple[float, float]) -> dict:
+
+    with open("worldcities.csv", "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        cities = list(reader)
+
+    nearest_city = None
+    min_distance = float("inf")
+    for city in cities:
+        city_coords = (float(city["lat"]), float(city["lng"]))
+        distance = geodesic(target_coords, city_coords).miles
+
+        if distance < min_distance:
+            min_distance = distance
+            nearest_city = city
+
+    if nearest_city is None:
+        return {}
+
+    return {
+        "city": nearest_city["city"],
+        "country": nearest_city["country"],
+        "latitude": nearest_city["lat"],
+        "longitude": nearest_city["lng"],
+        "distance": min_distance,
+    }
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <path_to_json>")
+        sys.exit(1)
+    JSON_path = sys.argv[1]
+    median_coords, total_distance = get_geometric_median(JSON_path)
+    print(f"Geometric Median Coordinates: {median_coords}")
+    city = find_nearest_city(median_coords)
+    print(city)
