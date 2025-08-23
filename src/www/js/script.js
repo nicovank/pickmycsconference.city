8a564e92ef74f7e7fbb4195d7f205f635a7ecc56
Maya V. Nedkova <mayavnedkova@gmail.com>
2025-08-22 20:12:03 -0400

    Script tweaks
    Altered functions for loading cluster points for ease-of-use instead of hardcoding json name
    
    Added maximum bounds to map to prevent user from excessively scrolling
    
    Altered code to parse new JSON format
    
    Added function to declare suggested location
    
    Added function to add suggested point based on provided JSON file

--- src/www/js/script.js
+++ src/www/js/script.js
@@ -6,21 +6,55 @@ Args: json_name (str): The name of the JSON file to fetch data from
 Returns: list: A list of GeoJSON features
 */
 
+let map;
+
+const redIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+async function addSuggestedCityMarker(data) {
+  const location = data.suggested_location;
+
+  const lat = Number(location.latitude);
+  const lon = Number(location.longitude);
+
+  var marker = L.marker([lat, lon], { icon: redIcon });
+  if (location.city) {
+    marker.bindPopup(location.city);
+  }
+  marker.addTo(map);
+}
+
 async function doFetch(json_name) {
   const features = [];
   const response = await fetch("./" + json_name);
   const data = await response.json();
-  for (let i = 0; i < data.length; i++) {
+
+  const submissions = data.happenings[0].submissions;
+
+  for (let i = 0; i < submissions.length; i++) {
     features.push({
       type: "Feature",
-      properties: { name: data[i].name },
+      properties: {
+        name: submissions[i].affiliation_name,
+      },
       geometry: {
-        coordinates: [data[i].longitude, data[i].latitude],
+        coordinates: [
+          submissions[i].location.longitude,
+          submissions[i].location.latitude,
+        ],
         type: "Point",
       },
     });
   }
-  return features;
+  return { features, data }; //return data so that we don't need to fetch it again
 }
 
 /* 
@@ -29,30 +63,38 @@ Create GeoJSON FeatureCollection from fetched data
 Returns: dict: A GeoJSON FeatureCollection
 */
 
-async function createGeojson() {
-  const features = await doFetch("sample.JSON");
+async function createGeojson(json_name = "data/FSE.json") {
+  const { features, data } = await doFetch(json_name);
   const geojson = {
     type: "FeatureCollection",
     features: features,
   };
-  return geojson;
+  return { geojson, data };
 }
 
 document.addEventListener("DOMContentLoaded", async function () {
   // Initialize map with a default view and set minimum zoom.
-  const map = L.map("map", { minZoom: 2 }).setView([0, 0], 2);
+
+  map = L.map("map", {
+    minZoom: 2,
+    maxBounds: [
+      [-90, -180],
+      [90, 180],
+    ],
+  }).setView([0, 0], 2);
 
   L.tileLayer(
     "https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=BqtwcjnWGsiCTa30jj5M",
     {
-      //I used the 'Streets' Map from MapTiler. Other formats can be found at https://cloud.maptiler.com/maps/
+      // I used the 'Streets' Map from MapTiler. Other formats can be found at https://cloud.maptiler.com/maps/
       attribution:
         '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
     },
   ).addTo(map);
 
-  const geojson = await createGeojson(); // Fetch and create GeoJSON data
+  const { geojson, data } = await createGeojson(); // Fetch and create GeoJSON data
   const markers = L.markerClusterGroup(); // Create a marker cluster group
+
   L.geoJSON(geojson, {
     pointToLayer: function (feature, latlng) {
       const marker = L.marker(latlng);
@@ -64,4 +106,6 @@ document.addEventListener("DOMContentLoaded", async function () {
   }).eachLayer((layer) => markers.addLayer(layer));
 
   map.addLayer(markers);
+
+  addSuggestedCityMarker(data);
 });
