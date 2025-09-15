0f83529ddf7127d3698bb4501097a9035d6045cb
Nicolas van Kempen <nvankemp@gmail.com>
2025-09-15 13:31:13 -0400

    Hide sensitive information :) Server will be decomissioned.

--- src/python/database_connection.py
+++ src/python/database_connection.py
@@ -1,11 +1,13 @@
+import os
+
 import psycopg2
 
 
 def open_connection() -> psycopg2.extensions.connection:
     return psycopg2.connect(
-        database="urv",
-        host="157.230.67.84",
-        user="urv",
-        password="urv-summer-2025",
-        port=5432,
+        database=os.getenv("DATABASE_NAME"),
+        host=os.getenv("DATABASE_HOST"),
+        user=os.getenv("DATABASE_USER"),
+        password=os.getenv("DATABASE_PASSWORD"),
+        port=int(os.getenv("DATABASE_PORT", "5432")),
     )
